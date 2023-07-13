78fe1b9e6eea2ce0324df127007d0f6ddf3a2022
Ogordiunor Uche Jude <excelxz78@gmail.com>
2023-07-13 12:04:35 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- * main - print a line of code using puts
+ *  main - print a line of code using puts
  *
- * Return: Always 0 (Success)
+ * Return: 0 (Success)
  */
 int main(void)
 {
