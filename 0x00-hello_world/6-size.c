e4a53f18fcd38b21ff6d43d142756938a55b3222
Ogordiunor Uche Jude <excelxz78@gmail.com>
2023-07-13 11:35:10 -0700

    Hello, c world programs

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+	int i;
+	long l;
+	long long ll;
+	float f;
+
+	printf("size of char: #lu byte(s)\n", size of (c));
+		printf("size of an  int: #lu byte(s)\n", size of (i));
+		printf("size of a long int: #lu byte(s)\n", size of (l));
+		printf("size of a long long int: #lu byte(s)\n", size of (ll));
+		printf("size of a float: #lu byte(s)\n", size of (f));
+    return (0);
+}
