b6a454c98c080f071207a0d071b4173224e055eb
Ogordiunor Uche Jude <excelxz78@gmail.com>
2023-07-14 08:41:59 -0700

    6-size.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+
 /**
- *  main - print a line of code using puts
+ * main - print a line of code using puts
  *
  * Return: Always 0 (Success)
  */
-int main(void)	
+int main(void)
 {
-puts("Programming is like building a multilingual puzzle");
-return 0;
+	puts("Programming is like building a multilingual puzzle");i
+	return (0);
 }
