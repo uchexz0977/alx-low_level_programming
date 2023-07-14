e8737e10e141206f9473487ca4ce7a36e0a10e9d
Ogordiunor Uche Jude <excelxz78@gmail.com>
2023-07-13 20:02:43 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,10 @@
 /**
  *  main - print a line of code using puts
  *
- * Return: 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");	
+puts("Programming is like building a multilingual puzzle");	
     return (0);
 }
